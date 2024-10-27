be6b591dec8275d5b83a7a66ae6c230153d99470
danishsheikh1122 <mohammeddanishsheikh1122@gmail.com>
2024-10-27 12:54:09 +0530

    git final upadre

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com','img.clerk.com', 'tse2.mm.bing.net','tse3.mm.bing.net'], // Add allowed domains here
-    },
-    webpack(config) {
-        // Add a rule for loading HTML files
-        config.module.rules.push({
-            test: /\.html$/,
-            use: 'html-loader', // Use the html-loader for HTML files
-        });
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "img.clerk.com",
+      "tse2.mm.bing.net",
+      "tse3.mm.bing.net",
+    ], // Add allowed domains here
+  },
+  webpack(config) {
+    // Add a rule for loading HTML files
+    config.module.rules.push({
+      test: /\.html$/,
+      use: "html-loader", // Use the html-loader for HTML files
+    });
 
-        return config;
-    },
-  };
-  
- export default nextConfig; // Properly export the nextConfig object
-  
+    return config;
+  },
+};
+
+export default nextConfig; // Properly export the nextConfig object
