fcfdbc069e18e1190b9ade5bc5561953525adb4e
Pravin nandankar <pravinnandankar03@gmail.com>
2024-10-27 12:35:32 +0530

    fixed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['res.cloudinary.com', 'tse2.mm.bing.net','tse3.mm.bing.net'], // Add allowed domains here
+        domains: ['res.cloudinary.com','img.clerk.com', 'tse2.mm.bing.net','tse3.mm.bing.net'], // Add allowed domains here
     },
     webpack(config) {
         // Add a rule for loading HTML files
